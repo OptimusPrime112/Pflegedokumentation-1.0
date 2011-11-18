d495b01661bc8cf654ac6340fa252fefb61eaad1
Ashanta <weitschat@t-online.de>
2011-11-18 18:39:23 +0100

    beginn
    Signed-off-by: Ashanta <weitschat@t-online.de>

---
+++ core.cpp
@@ -0,0 +1,6 @@
+*************************************************
+*** Copyright (c) 2011 Bunny Software Solution ***
+**************************************************
+//PreAlpha Version
+//Version:  0.0.1
+//Autor:  Marcus Weitschat
